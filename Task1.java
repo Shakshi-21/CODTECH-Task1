a69daf6e1d02aa317a01f2c1a675fd10f9731b1d
Shakshi Bhattacharya <140741338+Shakshi-21@users.noreply.github.com>
2024-08-27 12:27:27 +0530

    Task1.java

--- Task1.java
+++ Task1.java
@@ -1,4 +1,4 @@
-//Create a comprehensive Java program for an online banking system. This system should allow users to create accounts, deposit and withdraw funds, transfer money between accounts, view transaction history, and manage personal information. 
+/*Create a comprehensive Java program for an online banking system. This system should allow users to create accounts, deposit and withdraw funds, transfer money between accounts, view transaction history, and manage personal information*/
 
 import java.util.Scanner;
 import java.util.ArrayList;
